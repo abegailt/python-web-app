79ac6963dddff58a2118a1cbf72f8f11d73741dd
abegailt <abegail_tomaneng@trendmicro.com>
2016-06-12 10:23:08 +0800

    To do list with css

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-#Example of using Flask to make a simple TODO list
-
-app.py
-
-```python
 from flask import Flask, request
 from flask import render_template
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -15,41 +11,37 @@ db = SQLAlchemy(app)
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id      = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
-    done = db.Column(db.Boolean, default=False)
+    done    = db.Column(db.Boolean, default=False)
 
     def __init__(self, content):
         self.content = content
-        self.done = False
+        self.done    = False
 
     def __repr__(self):
         return '<Content %s>' % self.content
 
 
-db.drop_all()
 db.create_all()
 
 
 @app.route('/')
 def tasks_list():
     tasks = Task.query.all()
-    return render_template('list.html', tasks=tasks)
+    return render_template('testing5.html', tasks=tasks)
 
 
-@app.route('/task', methods=['POST'])
-def add_task():
-    content = request.form['content']
-    if not content:
-        return 'Error'
-
-    task = Task(content)
+#storing jsonjquery
+@app.route('/json')
+def background_process():
+    task2 = request.args.get('tasK')
+    task=Task(task2)
     db.session.add(task)
     db.session.commit()
     return redirect('/')
 
-
-@app.route('/delete/<int:task_id>')
+@app.route('/delete/<task_id>')
 def delete_task(task_id):
     task = Task.query.get(task_id)
     if not task:
@@ -59,23 +51,22 @@ def delete_task(task_id):
     db.session.commit()
     return redirect('/')
 
+@app.route('/edit/<int:task_id>')
+def edit_task_id(task_id):
+    details = Task.query.get(task_id)
+    tasks   = Task.query.all()
+    return render_template('edit.html',details=details ,tasks=tasks)
 
-@app.route('/done/<int:task_id>')
-def resolve_task(task_id):
-    task = Task.query.get(task_id)
-
-    if not task:
-        return redirect('/')
-    if task.done:
-        task.done = False
-    else:
-        task.done = True
-
+@app.route('/json2')
+def background_process2():
+    id              = request.args.get('id')
+    content         = request.args.get('tasK')
+    details         = Task.query.get(id)
+    details.content = content
+    db.session.add(details)
     db.session.commit()
-    return redirect('/')
+    return render_template('testing5.html',tasks=tasks)
 
 
 if __name__ == '__main__':
     app.run()
-
-```
